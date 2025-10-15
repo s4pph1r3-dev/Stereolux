60288da6cba631839d4ee524a0e55da3faf89def
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-10-15 13:41:28 +0200

    proto prog sur le téco

--- Assets/Scripts/ManagerGalleryBehaviour.cs
+++ Assets/Scripts/ManagerGalleryBehaviour.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using NUnit.Framework.Internal.Commands;
 
 public class ManagerGalleryBehaviour : MonoBehaviour
 {
     [SerializeField] List<GameObject> _LockSprite;
 
+    public static ManagerGalleryBehaviour Instance;
+
     private void Awake()
     {
-        SaveManager.SetAsFound(ItemType.PROGRAM, 0);
-        SaveManager.SetAsFound(ItemType.PROGRAM, 2);
-        SaveManager.SetAsFound(ItemType.PROGRAM, 6);
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
 
+    private void Start()
+    {
+        JustDoIt();
+    }
 
-
+    public void JustDoIt()
+    {
         for (int i = 0; i < _LockSprite.Count; i++)
         {
             if (SaveManager.HasBeenFound(ItemType.PROGRAM, i))
